ae2eade98baab3d0d5d9dbda7e527c3127d87421
Gabriel W. Bartmanovicz <obielwb@gmail.com>
2022-12-17 15:51:03 -0300

    test(content): add more tests

--- src/app/entities/content.spec.ts
+++ src/app/entities/content.spec.ts
@@ -1,14 +1,16 @@
 import { Content } from "./content";
 
-test('it should be able to create a notificaton content', () => {
-  const content = new Content('Hello World');
-  expect(content.value).toBe('Hello World');
-})
-
-test('it should not be able to create a notification content with less then 5 characters', () => {
-  expect(() => new Content('12')).toThrow('Content length must be between 5 and 255 characters');
-})
-
-test('it should not be able to create a notification content with more then 5 characters', () => {
-  expect(() => new Content('a'.repeat(256))).toThrow('Content length must be between 5 and 255 characters');
+describe('Notification content', () => {
+  it('should be able to create a notificaton content', () => {
+    const content = new Content('Hello World');
+    expect(content.value).toBe('Hello World');
+  })
+  
+  it('should not be able to create a notification content with less then 5 characters', () => {
+    expect(() => new Content('12')).toThrow('Content length must be between 5 and 255 characters');
+  })
+  
+  it('should not be able to create a notification content with more then 5 characters', () => {
+    expect(() => new Content('a'.repeat(256))).toThrow('Content length must be between 5 and 255 characters');
+  })
 })
