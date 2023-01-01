0a8e4645827ff53ebea6f960cf8d312b10b98750
Gabriel W. Bartmanovicz <obielwb@gmail.com>
2023-01-01 11:52:29 -0300

    chore(test): add missing values on in-memory-notification-repository

--- test/repositories/in-memory-notification-repository.ts
+++ test/repositories/in-memory-notification-repository.ts
@@ -2,7 +2,32 @@ import { Notification } from "@app/entities/notification";
 import { NotificationRepository } from "@app/repositories/notification-repository";
 
 class InMemoryNotificationRepository implements NotificationRepository {
+  
   public notifications: Notification[] = [];
+
+  async findById(notificationId: string): Promise<Notification | null> {
+    const notification = this.notifications.find(item => item.id === notificationId)
+    
+    if (!notification) {
+      return null;
+    }
+
+    return notification;
+  }
+
+  async countManyByRecipientId(recipientId: string): Promise<number> {
+    return this.notifications.filter(notification => notification.recipientId == recipientId).length
+  }
+
+  async save(notification: Notification): Promise<void> {
+    const notificationIndex = this.notifications.findIndex(
+      (item) => item.id === notification.id
+    )
+
+    if (notificationIndex >= 0) {
+      this.notifications[notificationIndex] = notification
+    }
+  }
 
   async create(notification: Notification): Promise<void> {
     this.notifications.push(notification);
