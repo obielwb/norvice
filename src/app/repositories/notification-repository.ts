386d4f3ed8dc4cb8466dd464e501b3f4cba5990a
Gabriel W. Bartmanovicz <obielwb@gmail.com>
2023-01-01 10:14:54 -0300

    feat(repositories): add find by id on notification

--- src/app/repositories/notification-repository.ts
+++ src/app/repositories/notification-repository.ts
@@ -2,6 +2,7 @@ import { Notification } from "../entities/notification";
 
 abstract class NotificationRepository {
   abstract create(notification: Notification): Promise<void>;
+  abstract findById(notificationId: string): Promise<Notification | null>;
 }
 
 export { NotificationRepository };
